73ca2fd149a859595faa876846b32c8c20e75311
Patrick Judge <pjudge@bettergov.org>
2019-06-10 12:04:40 -0500

    format projects as table

--- ohare-promises/src/js/Inner.svelte
+++ ohare-promises/src/js/Inner.svelte
@@ -14,19 +14,77 @@
   h2 {
     text-align: center;
   }
+
+  h3[data-year] {
+    position: relative;
+  }
+
+  h3[data-year]::before {
+    content: attr(data-year);
+    color: slategray;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  @media (min-width: 1160px) {
+    h3[data-year]::before {
+      margin-right: 1rem;
+      position: absolute;
+      right: 100%;
+    }
+  }
+
+  table {
+    table-layout: fixed;
+    width: 100%;
+  }
+
+  table td:first-child,
+  table th:first-child {
+    width: 11.05ch; /* width of "Description" */
+  }
+
+  table td,
+  table th {
+    padding: 0.5rem;
+    hyphens: auto;
+  }
+
+  .project-individual {
+    margin-bottom: 3rem;
+  }
 </style>
 
 <div class="projects-container">
   <h2>{title}</h2>
 
-  {#each projects as { description, promised, delivered }}
-    <h3>{description}</h3>
+  {#each projects as { date, description, promised, delivered }}
+    <div class="project-individual">
+      <h3 data-year={date}>{description}</h3>
 
-    {#each [promised, delivered] as { title, cost, date, description }}
-      <h4>{title}</h4>
-      <p>Cost: {cost}</p>
-      <p>Date: {date}</p>
-      <p>Description: {description}</p>
-    {/each}
+      <table lang="en-us">
+        <thead>
+          <th />
+          <th>{promised.title}</th>
+          <th>{delivered.title}</th>
+        </thead>
+        <tr>
+          <td>Date</td>
+          <td>{promised.date}</td>
+          <td>{delivered.date}</td>
+        </tr>
+        <tr>
+          <td>Cost</td>
+          <td>{promised.cost}</td>
+          <td>{delivered.cost}</td>
+        </tr>
+        <tr>
+          <td>Description</td>
+          <td>{promised.description}</td>
+          <td>{delivered.description}</td>
+        </tr>
+      </table>
+
+    </div>
   {/each}
 </div>
