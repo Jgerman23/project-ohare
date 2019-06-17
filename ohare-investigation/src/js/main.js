595e1dfdfa0935e645ce1b6c0340b54a486c09ce
Patrick Judge <pjudge@bettergov.org>
2019-06-17 13:06:32 -0500

    clean styles

--- ohare-investigation/src/js/main.js
+++ ohare-investigation/src/js/main.js
@@ -2,24 +2,7 @@ import '../scss/main.scss';
 import './share';
 import '@fortawesome/fontawesome-free/css/solid.css';
 
-// import App from './App.svelte';
-
-// const app = new App({
-//   target: document.querySelector('article.main'),
-//   anchor: document.querySelector('section.story'),
-//   props: {
-//     name: 'world'
-//   }
-// });
-
-// window.app = app;
-
-// export default app;
-
 document.addEventListener('DOMContentLoaded', function() {
   const hed = document.querySelector('.hed');
-
-  // hed.
-  // console.log(hed);
   hed.innerHTML = hed.innerHTML.replace(/\S/g, '<b>$&</b>');
 });
