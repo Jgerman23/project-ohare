c7d4c538f4f384bdcf58f5e9ae0539095d3ffdac
Patrick Judge <pjudge@bettergov.org>
2019-06-17 18:08:59 -0500

    fix heading-terminal init

--- ohare-investigation/src/js/heading-terminal.js
+++ ohare-investigation/src/js/heading-terminal.js
@@ -1,6 +1,30 @@
 import debounce from 'debounce';
 
-const onResize = function(selector) {
+const getNextSibling = function(elem, selector) {
+  // Get the next sibling element
+  var sibling = elem.nextElementSibling;
+
+  // If the sibling matches our selector, use it
+  // If not, jump to the next sibling and continue the loop
+  while (sibling) {
+    if (sibling.matches(selector)) return sibling;
+    sibling = sibling.nextElementSibling;
+  }
+};
+
+const getPrevSibling = function(elem, selector) {
+  // Get the next sibling element
+  var sibling = elem.previousElementSibling;
+
+  // If the sibling matches our selector, use it
+  // If not, jump to the next sibling and continue the loop
+  while (sibling) {
+    if (sibling.matches(selector)) return sibling;
+    sibling = sibling.previousElementSibling;
+  }
+};
+
+export const onResize = function(selector) {
   const headings = document.querySelectorAll(selector);
 
   if (!headings.length) return;
@@ -16,12 +40,14 @@ const onResize = function(selector) {
 
     spaces.forEach(spaceEl => {
       if (
-        spaceEl.getBoundingClientRect().top !==
-        spaceEl.nextElementSibling.getBoundingClientRect().top
+        spaceEl.getBoundingClientRect().top ===
+          getNextSibling(spaceEl, 'div').getBoundingClientRect().top &&
+        spaceEl.getBoundingClientRect().top ===
+          getPrevSibling(spaceEl, 'div').getBoundingClientRect().top
       ) {
-        spaceEl.classList.add('invisible');
-      } else {
         spaceEl.classList.remove('invisible');
+      } else {
+        spaceEl.classList.add('invisible');
       }
     });
   });
@@ -33,6 +59,7 @@ const init = function(selector) {
   terminalHeadings.forEach(hed => {
     const titleHTML = hed.innerHTML
       .replace(/<[^>]*>?/gm, '') // strip existing html
+      .replace(/&nbsp;/g, ' ') // strip existing &nbsp;
       .replace(/\S/g, '<b>$&</b>')
       .replace(
         /\s/g,
