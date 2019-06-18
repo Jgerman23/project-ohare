7fb6eb1abb8a26ff708fdc5f5f9959e7d809c2a6
Patrick Judge <pjudge@bettergov.org>
2019-06-18 18:06:31 -0500

    make hydratable

--- ohare-promises/src/js/main.js
+++ ohare-promises/src/js/main.js
@@ -2,9 +2,12 @@ import '../scss/main.scss';
 import App from './App.svelte';
 import 'ohare-investigation/src/js/main';
 
+const mode = process.env.NODE_ENV || 'development';
+const prod = mode === 'production';
+
 const app = new App({
-  target: document.querySelector('article.main'),
-  // anchor: document.querySelector('section.story'),
+  target: document.querySelector('#svelte-app'),
+  hydrate: prod ? true : false,
   props: {
     name: 'world'
   }
