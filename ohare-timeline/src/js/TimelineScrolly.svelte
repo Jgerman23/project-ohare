f10cd44f30d083cc4b43b64c92d9f470626064c0
Patrick Judge <pjudge@bettergov.org>
2019-06-19 18:01:07 -0500

    add button navigation

--- ohare-timeline/src/js/TimelineScrolly.svelte
+++ ohare-timeline/src/js/TimelineScrolly.svelte
@@ -1,5 +1,11 @@
 <script context="module">
+  import { slide } from "./stores";
+
+  let allContainers = [];
+
   const onLoad = () => {
+    allContainers = document.querySelectorAll(".tl-event-container");
+
     const eventItems = document.querySelectorAll(
       ".tl-event-container .tl-text"
     );
@@ -8,6 +14,7 @@
       entries.forEach(entry => {
         if (entry.isIntersecting) {
           entry.target.parentNode.classList.add("is-active");
+          slide.set(entry.target.parentNode);
         } else {
           entry.target.parentNode.classList.remove("is-active");
         }
@@ -24,6 +31,7 @@
       entries.forEach(entry => {
         if (entry.isIntersecting) {
           document.querySelector(".timeline").classList.add("hide-all-images");
+          // slide.set(entry.target);
         } else {
           document
             .querySelector(".timeline")
@@ -41,9 +49,44 @@
 </script>
 
 <script>
-  import TimelineEvents from "./TimelineEvents.svelte";
+  let currentSlide, currentIndex;
 
+  const unsubscribe = slide.subscribe(value => {
+    currentSlide = value;
+    currentIndex = Array.prototype.indexOf.call(
+      document.querySelectorAll(".tl-event-container"),
+      currentSlide
+    );
+  });
+
+  import { onDestroy } from "svelte";
+
+  onDestroy(unsubscribe);
+
+  import TimelineEvents from "./TimelineEvents.svelte";
   export let content = [];
+
+  const onClickPrev = () => {
+    const prevEl = allContainers.item(currentIndex - 1);
+
+    if (prevEl)
+      prevEl.querySelector(".tl-text").scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+        inline: "nearest"
+      });
+  };
+
+  const onClickNext = () => {
+    const nextEl = allContainers.item(currentIndex + 1);
+
+    if (nextEl)
+      nextEl.querySelector(".tl-text").scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+        inline: "nearest"
+      });
+  };
 </script>
 
 <style lang="scss">
@@ -102,7 +145,49 @@
     display: flex;
     align-items: center;
   }
+
+  .controls {
+    position: fixed;
+    bottom: 3.5rem;
+    // left: 0;
+    right: 1rem;
+    text-align: right;
+    z-index: 99;
+  }
+
+  button {
+    background: none;
+    color: ivory;
+    text-transform: uppercase;
+    border: 2px solid currentColor;
+    font-size: 1rem;
+    margin: 0 0.5rem;
+    transition: background 0.3s ease, opacity 0.3s ease;
+    padding: 0.5rem;
+  }
+
+  button:hover,
+  button:focus {
+    background: rgba(255, 255, 255, 0.4);
+  }
+
+  button[disabled] {
+    pointer-events: none;
+    opacity: 0;
+  }
 </style>
+
+<div class="controls">
+  <button on:click={onClickPrev} disabled={currentIndex <= 0}>
+    <i class="fas fa-chevron-up" />
+  </button>
+  <button
+    on:click={onClickNext}
+    disabled={currentIndex == allContainers.length - 1}>
+    Next
+    <i class="fas fa-chevron-down" />
+  </button>
+</div>
 
 <div class="timeline is-scrolly" id="timeline-inner">
   {#each content as contentItem}
