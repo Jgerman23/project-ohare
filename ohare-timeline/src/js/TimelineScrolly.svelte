adaa8fb4b9fe8196a3191c1a65e7398d90b56091
Patrick Judge <pjudge@bettergov.org>
2019-06-19 18:49:09 -0500

    add toggle for scrolly vs compact

--- ohare-timeline/src/js/TimelineScrolly.svelte
+++ ohare-timeline/src/js/TimelineScrolly.svelte
@@ -55,6 +55,8 @@
 </script>
 
 <script>
+  import Switch from "./Switch";
+
   let currentSlide, currentIndex;
 
   const unsubscribe = slide.subscribe(value => {
@@ -181,87 +183,26 @@
     pointer-events: none;
     opacity: 0;
   }
-
-  /* The switch - the box around the slider */
-  .switch {
-    position: relative;
-    display: inline-block;
-    width: 60px;
-    height: 34px;
-  }
-
-  /* Hide default HTML checkbox */
-  .switch input {
-    opacity: 0;
-    width: 0;
-    height: 0;
-  }
-
-  /* The slider */
-  .slider {
-    position: absolute;
-    cursor: pointer;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: #ccc;
-    -webkit-transition: 0.4s;
-    transition: 0.4s;
-  }
-
-  .slider:before {
-    position: absolute;
-    content: "";
-    height: 26px;
-    width: 26px;
-    left: 4px;
-    bottom: 4px;
-    background-color: white;
-    -webkit-transition: 0.4s;
-    transition: 0.4s;
-  }
-
-  input:checked + .slider {
-    background-color: #2196f3;
-  }
-
-  input:focus + .slider {
-    box-shadow: 0 0 1px #2196f3;
-  }
-
-  input:checked + .slider:before {
-    -webkit-transform: translateX(26px);
-    -ms-transform: translateX(26px);
-    transform: translateX(26px);
-  }
-
-  /* Rounded sliders */
-  .slider.round {
-    border-radius: 34px;
-  }
-
-  .slider.round:before {
-    border-radius: 50%;
-  }
 </style>
 
-<div class="controls">
-  <button on:click={onClickPrev} disabled={currentIndex <= 0}>
-    <i class="fas fa-chevron-up" />
-  </button>
-  <button
-    on:click={onClickNext}
-    disabled={currentIndex == allContainers.length - 1}>
-    Next
-    <i class="fas fa-chevron-down" />
-  </button>
-</div>
+{#if showScrolly}
+  <div class="controls">
+    <button on:click={onClickPrev} disabled={currentIndex <= 0}>
+      <i class="fas fa-chevron-up" />
+    </button>
+    <button
+      on:click={onClickNext}
+      disabled={currentIndex == allContainers.length - 1}>
+      Next
+      <i class="fas fa-chevron-down" />
+    </button>
+  </div>
+{/if}
 
-<!-- <label class="switch">
-  <input type="checkbox" bind:checked={showScrolly} />
-  <span class="slider round" />
-</label> -->
+<!-- <p>
+  Show rich media:
+  <Switch bind:checked={showScrolly} />
+</p> -->
 
 <div class="timeline" class:is-scrolly={showScrolly} id="timeline-inner">
   {#each content as contentItem}
