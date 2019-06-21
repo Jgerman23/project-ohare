a8957455b30a0ae0b1c0971e7551620893328eb6
Patrick Judge <pjudge@bettergov.org>
2019-06-21 13:24:12 -0500

    upgrade to new pit template

--- ohare-timeline/webpack.config.js
+++ ohare-timeline/webpack.config.js
@@ -2,9 +2,34 @@ const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const sharp = require('sharp');
 
 const mode = process.env.NODE_ENV || 'development';
 const prod = mode === 'production';
+
+const responsiveImages = (widths, opts = {}) => {
+  const minWidth = opts.minWidth || 800;
+
+  return widths.map(width => {
+    let newWidth = Math.max(width, minWidth);
+
+    return {
+      from: 'src/static/images',
+      to: 'images',
+      transform: content =>
+        sharp(content)
+          .resize(newWidth)
+          .toBuffer(),
+      transformPath(targetPath, absolutePath) {
+        var arrPath = targetPath.split('.');
+        var ext = arrPath.pop();
+        var path = arrPath.join('.');
+        return `${path}-${width}.${ext}`;
+      }
+    };
+  });
+};
 
 let webpackConfig = {
   entry: {
@@ -56,6 +81,13 @@ let webpackConfig = {
     new MiniCssExtractPlugin({
       filename: '[name].[hash].css'
     }),
+    new CopyPlugin([
+      {
+        from: 'src/static',
+        to: ''
+      },
+      ...responsiveImages([400, 800, 1200, 1800])
+    ]),
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/) // ignore moment locales
   ],
   devtool: prod ? false : 'inline-cheap-source-map',
@@ -68,7 +100,6 @@ const glob = require('glob');
 const TerserJSPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const PurgeCssPlugin = require('purgecss-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
 const PrerenderSPAPlugin = require('prerender-spa-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
@@ -83,9 +114,8 @@ prodConfig = {
   plugins: [
     new PurgeCssPlugin({
       paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
-      whitelistPatterns: [/^svelte-/, /^u-/]
+      whitelistPatterns: [/^svelte-/]
     }),
-    new CopyPlugin([{ from: 'src/static', to: '' }]),
     new PrerenderSPAPlugin({
       staticDir: path.join(__dirname, 'public'),
       routes: ['/']
