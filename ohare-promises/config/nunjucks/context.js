d86247e1c29757c105cc1aeb38a2e914cad96af3
Patrick Judge <pjudge@bettergov.org>
2019-06-21 13:15:26 -0500

    load in new pit template

--- ohare-promises/config/nunjucks/context.js
+++ ohare-promises/config/nunjucks/context.js
@@ -1,5 +1,9 @@
 const fs = require('fs-extra');
 const path = require('path');
+const getMeta = require(path.resolve(process.cwd(), 'meta.js'));
+const sizeOf = require('image-size');
+const readChunk = require('read-chunk');
+const imageType = require('image-type');
 
 module.exports = {
   getContext: () => {
@@ -10,10 +14,35 @@ module.exports = {
       path.resolve(process.cwd(), 'src/data/data.json')
     );
 
+    var shareImageUrl = "O'Hare- Projects.jpg";
+    var shareImagePath = path.resolve(
+      process.cwd(),
+      'src/static/',
+      shareImageUrl
+    );
+    var dimensions = sizeOf(shareImagePath);
+    var buffer = readChunk.sync(shareImagePath, 0, 12);
+
+    const contextMeta = getMeta({
+      title: contextData.COPY.seo_headline,
+      description: contextData.COPY.subhed,
+      updatedProps: {
+        share: {
+          image: {
+            url: shareImageUrl,
+            width: dimensions.width,
+            height: dimensions.height,
+            type: imageType(buffer).mime
+          }
+        }
+      }
+    });
+
     const templateContext = {
       ARCHIE: contextArchie,
       DATA: contextData,
-      ENV: process.env.NODE_ENV
+      ENV: process.env.NODE_ENV,
+      META: contextMeta
     };
 
     return templateContext;
