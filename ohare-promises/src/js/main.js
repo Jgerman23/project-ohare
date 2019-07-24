675bb2374957d0e844d71815057650b8afc8ad72
Patrick Judge <pjudge@bettergov.org>
2019-07-24 12:57:29 -0500

    Load analytics in head

--- ohare-promises/src/js/main.js
+++ ohare-promises/src/js/main.js
@@ -5,9 +5,6 @@ import 'ohare-investigation/src/js/main';
 const mode = process.env.NODE_ENV || 'development';
 const prod = mode === 'production';
 
-// Load custom tracking code lazily, so it's non-blocking
-import('./analytics.js').then(analytics => analytics.init());
-
 const app = new App({
   target: document.querySelector('#svelte-app'),
   hydrate: prod ? true : false,
