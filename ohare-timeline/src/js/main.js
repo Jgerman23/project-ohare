0760b93b90ebf3c89a1418acafc6e18e2e5dc58c
Patrick Judge <pjudge@bettergov.org>
2019-07-24 12:43:12 -0500

    Load GA with global tag in head

--- ohare-timeline/src/js/main.js
+++ ohare-timeline/src/js/main.js
@@ -11,9 +11,6 @@ import 'lazysizes/plugins/blur-up/ls.blur-up';
 const mode = process.env.NODE_ENV || 'development';
 const prod = mode === 'production';
 
-// Load custom tracking code lazily, so it's non-blocking
-import('./analytics.js').then(analytics => analytics.init());
-
 const app = new Timeline({
   target: document.querySelector('#timeline-app'),
   hydrate: prod ? true : false,
